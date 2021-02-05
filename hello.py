c77f7a57c86bde086b83979bd9ba20eec3708fe4
Mayel <pelle.mayel@gmail.com>
2021-02-05 09:31:36 +0100

    Jinja2 template

--- hello.py
+++ hello.py
@@ -1,18 +1,9 @@
-from flask import Flask, redirect, url_for, request
+from flask import Flask, render_template
 app = Flask(__name__)
 
-@app.route('/success/<name>')
-def success(name):
-   return 'welcome %s' % name
-
-@app.route('/login',methods = ['POST', 'GET'])
-def login():
-   if request.method == 'POST':
-      user = request.form['nm']
-      return redirect(url_for('success',name = user))
-   else:
-      user = request.args.get('nm')
-      return redirect(url_for('success',name = user))
+@app.route('/hello/<user>')
+def hello_name(user):
+   return render_template('hello.html', name = user)
 
 if __name__ == '__main__':
    app.run(debug = True)
